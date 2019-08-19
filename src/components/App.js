87ba996eecfc3dbe1d1920adee79f57a5b68620b
Gao <gaotest87@gmail.com>
2019-08-19 14:03:48 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+// import DraggableTest from 'Components/DraggableTest'
+
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app" style={{ height: '100vh', width: '100vw' }}>
+      Hello how are you This is going well so far well are you man
+    </div>
+  )
+}
+
+export default hot(App)
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin')
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: ['webpack-hot-middleware/client', './src/index.js'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', {
+            loader: 'sass-loader',
+            options: { implementation: require('sass') }, // eslint-disable-line global-require
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Utils: path.resolve(__dirname, 'src/utils'),
+    },
+    plugins: [
+      new DirectoryNamedWebpackPlugin(),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+  },
+}
